62ffd66cedb3cfe50128dd29ae82730782d4b09d
Kartik Bhadouriya <ks8783375@gmail.com>
2025-03-01 12:14:32 -0800

    Update ShopContext.jsx

--- froentend/src/Context/ShopContext.jsx
+++ froentend/src/Context/ShopContext.jsx
@@ -7,7 +7,7 @@ export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
     const currency = '₹';
     const delivery_fee = 100;
-    const backendUrl = https://full-stack-ecommerce-website-back.vercel.app
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const [search, setSearch] = useState('')
     const [showSearch, setShowSearch] = useState(false)
